f64dde6fc4cfb6115cbecbd01305332cdf2f4129
Syngmaster <max6361@mail.ru>
2017-07-29 21:18:47 +0100

    Initial Commit

---
+++ TextFieldValidationMethods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextFieldValidationMethods
+//
+//  Created by Syngmaster on 29/07/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextFieldValidationMethods/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TextFieldValidationMethods
+//
+//  Created by Syngmaster on 29/07/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
